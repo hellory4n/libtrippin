9223a1eae31da49cda316acb4940036ada753241
hellory4n <hellory4n@gmail.com>
2025-05-07 20:28:47 -0500

    I called my doctor, and said some really crazy shit.

--- engineer/engineer.lua
+++ engineer/engineer.lua
@@ -1,2 +1,12 @@
 eng = require("libengineer")
 eng.init()
+
+eng.recipe("build", function()
+	print("Sigma")
+end)
+
+eng.option("sigma", function(val)
+	print("sigma on th e wal :" .. val)
+end)
+
+eng.run()
--- engineer/libengineer.lua
+++ engineer/libengineer.lua
@@ -30,8 +30,12 @@ eng = {
 	-- C++ compiler
 	cxx = "",
 
-	-- constants
+	-- Table of strings with functions (no arguments, no returns)
+	recipes = {},
+	-- Table of strings with functions (no arguments, no returns)
+	options = {},
 
+	-- constants
 	CONSOLE_COLOR_RESET = "\27[0m",
 	CONSOLE_COLOR_LIB = "\27[0;90m",
 	CONSOLE_COLOR_WARN = "\27[0;93m",
@@ -42,6 +46,86 @@ eng = {
 function eng.util.silentexec(command)
 	-- TODO this will definitely break at some point
 	return os.execute(command .. " > /dev/null 2>&1")
+end
+
+-- Prints a table for debugging. This entire function is stolen.
+function eng.util.print_table(node)
+    local cache, stack, output = {},{},{}
+    local depth = 1
+    local output_str = "{\n"
+
+    while true do
+        local size = 0
+        for k,v in pairs(node) do
+            size = size + 1
+        end
+
+        local cur_index = 1
+        for k,v in pairs(node) do
+            if (cache[node] == nil) or (cur_index >= cache[node]) then
+
+                if (string.find(output_str,"}",output_str:len())) then
+                    output_str = output_str .. ",\n"
+                elseif not (string.find(output_str,"\n",output_str:len())) then
+                    output_str = output_str .. "\n"
+                end
+
+                -- This is necessary for working with HUGE tables otherwise we run out of memory using concat on huge strings
+                table.insert(output,output_str)
+                output_str = ""
+
+                local key
+                if (type(k) == "number" or type(k) == "boolean") then
+                    key = "["..tostring(k).."]"
+                else
+                    key = "['"..tostring(k).."']"
+                end
+
+                if (type(v) == "number" or type(v) == "boolean") then
+                    output_str = output_str .. string.rep('\t',depth) .. key .. " = "..tostring(v)
+                elseif (type(v) == "table") then
+                    output_str = output_str .. string.rep('\t',depth) .. key .. " = {\n"
+                    table.insert(stack,node)
+                    table.insert(stack,v)
+                    cache[node] = cur_index+1
+                    break
+                else
+                    output_str = output_str .. string.rep('\t',depth) .. eng.CONSOLE_COLOR_RESET .. key .. eng.CONSOLE_COLOR_RESET .. " = '"..tostring(v).."'"
+                end
+
+                if (cur_index == size) then
+                    output_str = output_str .. "\n" .. string.rep('\t',depth-1) .. "}"
+                else
+                    output_str = output_str .. ","
+                end
+            else
+                -- close the table
+                if (cur_index == size) then
+                    output_str = output_str .. "\n" .. string.rep('\t',depth-1) .. "}"
+                end
+            end
+
+            cur_index = cur_index + 1
+        end
+
+        if (size == 0) then
+            output_str = output_str .. "\n" .. string.rep('\t',depth-1) .. "}"
+        end
+
+        if (#stack > 0) then
+            node = stack[#stack]
+            stack[#stack] = nil
+            depth = cache[node] == nil and depth + 1 or depth - 1
+        else
+            break
+        end
+    end
+
+    -- This is necessary for working with HUGE tables otherwise we run out of memory using concat on huge strings
+    table.insert(output,output_str)
+    output_str = table.concat(output)
+
+    print(output_str)
 end
 
 -- Initializes engineer™
@@ -65,7 +149,7 @@ function eng.init()
 	end
 	print(eng.CONSOLE_COLOR_LIB .. "C compiler: " .. eng.cc .. eng.CONSOLE_COLOR_RESET)
 
-	-- get c compiler
+	-- get c++ compiler
 	local cxx = os.getenv("CXX") or os.getenv("CPP")
 	if cxx ~= nil and cxx ~= "" then
 		eng.cxx = cxx
@@ -81,6 +165,55 @@ function eng.init()
 		end
 	end
 	print(eng.CONSOLE_COLOR_LIB .. "C++ compiler: " .. eng.cxx .. eng.CONSOLE_COLOR_RESET)
+
+	-- TODO default "help" recipe
+end
+
+-- Makes a recipe. The callback will be called when the recipe is used.
+function eng.recipe(name, callback)
+	eng.recipes[name] = callback
+end
+
+-- Adds an option. The callback takes in whatever value the option has (string), and is only called
+-- if that option is used.
+function eng.option(name, callback)
+	eng.options[name] = callback
+end
+
+-- Put this at the end of your build script so it actually does something
+function eng.run()
+	opts = {}
+	recipes = {}
+
+	for _, argma in ipairs(arg) do
+		local key, val = argma:match("^(%w+)=?(%S*)$")
+		-- man.
+		if key == "" then key = nil end
+		if val == "" then val = nil end
+
+		-- we have to sort options and recipes so options are set before recipes
+		if key and val then
+			table.insert(opts, {key = key, val = val})
+		elseif key then
+			table.insert(recipes, key)
+		end
+	end
+
+	for _, mate in ipairs(opts) do
+		if eng.options[mate.key] ~= nil then
+			eng.options[mate.key](mate.val)
+		else
+			print(eng.CONSOLE_COLOR_WARN .. "unknown option \"" .. mate.key .. "\"" .. eng.CONSOLE_COLOR_RESET)
+		end
+	end
+
+	for _, recipema in ipairs(recipes) do
+		if eng.recipes[recipema] ~= nil then
+			eng.recipes[recipema]()
+		else
+			print(eng.CONSOLE_COLOR_WARN .. "unknown recipe \"" .. recipema .. "\"" .. eng.CONSOLE_COLOR_RESET)
+		end
+	end
 end
 
 return eng
